f2eb50d4fb44cb8f5dcd004e8e65c6277df43952
BrendrWood <brendrwood@gmail.com>
2021-08-05 18:21:56 +0300

    Initial import

---
+++ src/test/java/BrendrWood/TestLogInTest.java
@@ -0,0 +1,51 @@
+package BrendrWood;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestLogInTest extends BrendrWood.TestNgTestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testUntitled() throws Exception {
+    driver.get("http://localhost/php4dvd-master/");
+    driver.manage().window().setSize(new Dimension(968, 1040));
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.id("password")).sendKeys("admin");
+    driver.findElement(By.cssSelector(".btn-block")).click();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
+
