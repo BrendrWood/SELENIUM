67c7ebebe8c17dfba3eb064851bebe3ac78e4828
BrendrWood <brendrwood@gmail.com>
2021-08-06 16:09:54 +0300

    Still do not work

--- src/test/java/BrendrWood/TestLogInTest.java
+++ src/test/java/BrendrWood/TestLogInTest.java
@@ -4,11 +4,12 @@ import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.*;
 import org.testng.annotations.*;
-
+//import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.Select;
+
 
 public class TestLogInTest extends BrendrWood.TestNgTestBase {
   private boolean acceptNextAlert = true;
